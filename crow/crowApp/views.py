518ca7a2b542f54f6731350ca22e1871d852b665
Amine-Trabelsi <aminetr76@gmail.com>
2023-06-02 15:10:21 +0300

    displaying available models in template

--- crow/crowApp/views.py
+++ crow/crowApp/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import Http404
 # Create your views here.
 
+from .models import CrowPost
+
 def index(request):
     # to do: add context
+    posts = CrowPost.objects.order_by("published_date")
     context = {
         "Author": "Amine",
+        "posts": posts, 
     }
     return render(request, "crowApp/index.html", context)
