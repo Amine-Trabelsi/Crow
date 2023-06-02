278cabf3d0b6eb67269a45121ee46e9720168114
Amine-Trabelsi <aminetr76@gmail.com>
2023-06-02 13:48:12 +0300

    Create a model and a template

--- crow/crowApp/views.py
+++ crow/crowApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # to do: add context
+    context = {
+        "Author": "Amine",
+    }
+    return render(request, "crowApp/index.html", context)
